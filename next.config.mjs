507980de189c3f277041ed60c028585b89d0874e
Enzo Castro <enzocastro95600@gmail.com>
2024-11-24 14:54:37 +0100

    Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // output: 'export', // export statique
+    output: 'export', // export statique
 };
 
 export default nextConfig;
